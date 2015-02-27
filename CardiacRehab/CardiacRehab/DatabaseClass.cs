3a7a62ab4862c9b4baf84a4116cddd32e541559f
Kieran92 <kboyle@ualberta.ca>
2015-02-27 14:54:17 -0700

    IP updated for computer #2

--- CardiacRehab/CardiacRehab/DatabaseClass.cs
+++ CardiacRehab/CardiacRehab/DatabaseClass.cs
@@ -26,7 +26,7 @@ namespace CardiacRehab
         /// </summary>
         private void InitializeDB()
         {
-            db_server = "localhost";
+            db_server = "192.168.184.33";
             db_name = "cardiac";
             db_username = "root";
             db_password = "";
